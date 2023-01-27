c1d888a39171ee3a45a69a396a408e3efb04df20
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-01-27 15:40:45 -0500

    refactorizacion configuracion angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {canActivate,redirectLoggedInTo} from '@angular/fire/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: () =>
+			import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    ...canActivate(()=>redirectLoggedInTo(['card-shop']))
+	},
+	{
+		path: '**',
+		redirectTo: 'auth',
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
