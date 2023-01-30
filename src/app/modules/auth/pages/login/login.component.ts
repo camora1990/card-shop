728b49df9eb0c6a370e153e443bbfd792f4407d3
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-01-30 10:24:00 -0500

    funcion de login

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 	title: string = 'Cards shop';
 	subTitle: string = 'Available now';
-  placeHolder:string = "Sign in with google"
-	constructor() {}
+	placeHolder: string = 'Sign in with google';
+	suscriptions: Subscription[] = [];
+	constructor(private $auth: AuthService, private route: Router) {}
+	ngOnDestroy(): void {
+		this.suscriptions.forEach((s) => s.unsubscribe());
+	}
 
 	ngOnInit(): void {}
+
+	login(event: MouseEvent) {
+		const suscription = this.$auth.loginGoogle().subscribe({
+			next: (_) => {
+				this.route.navigate(['card-shop/home']);
+			},
+		});
+		this.suscriptions.push(suscription);
+	}
 }
