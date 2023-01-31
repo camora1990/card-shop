9ed8dcb778b971573ca8ed74006c63f99c617e9a
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-01-31 12:41:03 -0500

    refactorizacion usuario

--- src/app/modules/core/domain/entities/historyChange.model.ts
+++ src/app/modules/core/domain/entities/historyChange.model.ts
@@ -1,9 +1,10 @@
 
 import { HistoryType } from '../enums/historyType.model';
-import { User } from './user.model';
+import { UserDto } from './userDto';
+
 
 export interface HistoryChange {
 	uid: string;
 	type: HistoryType;
-	owner: User;
+	owner: UserDto;
 }
