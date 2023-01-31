358018cef054467eb2d4ccd878961cdae0db14e5
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-01-31 12:41:46 -0500

    refactorizacion codigo

--- src/app/modules/core/services/card.service.ts
+++ src/app/modules/core/services/card.service.ts
@@ -10,10 +10,23 @@ import {
 	where,
 } from '@angular/fire/firestore';
 
-import { collection } from '@firebase/firestore';
-import { Observable, tap, throwError, switchMap, of, from } from 'rxjs';
+import { collection, DocumentReference } from '@firebase/firestore';
+import {  uuidv4} from "@firebase/util";
+import {
+	Observable,
+	throwError,
+	switchMap,
+	of,
+	from,
+	take,
+	zip,
+	mergeMap,
+} from 'rxjs';
 import { Card } from '../domain/entities/card.model';
 import { UserService } from './user.service';
+import { HistoryChange } from '../domain/entities/historyChange.model';
+import { HistoryType } from '../domain/enums/historyType.model';
+import { User } from '../domain/entities/user.model';
 
 @Injectable({
 	providedIn: 'root',
@@ -37,12 +50,51 @@ export class CardService {
 	}
 
 	public buyCard(card: Card) {
-		const cardReference = doc(this.refCards, card.uid)
-		return this.$user.getUser("5pygPrNvlkSZ9koFYPD41r0iyTX2")
+		const cardReference = doc(this.refCards, card.uid);
+		return this.$user.getUser(this.$user.currenUser?.uid!).pipe(
+			take(1),
+			switchMap((user) => {
+				if (user[0].balance < card.price)
+					return throwError(() => `Not enough balance`);
+				return of(user);
+			}),
+			switchMap((user) => {
+				return zip(this.getCard(card.uid), of(user));
+			}),
+			mergeMap(([card, user]) => {
+				return this.validateBuyCard(card,user[0],cardReference)
+			}),
+		);
 	}
 
 	public getCard(uid: string): Observable<Card> {
 		const cards = doc(this.$firestore, `cards/${uid}`);
 		return docData(cards, { idField: 'uid' }) as Observable<Card>;
+	}
+
+	private validateBuyCard(card:Card,user:User, cardReference:DocumentReference){
+		if (card.activeForSale) {
+			const {avatar,uid,email,username} = user
+			const history: HistoryChange={
+				owner: {avatar,email,uid,username},
+				type: HistoryType.PURCHASE,
+				uid: uuidv4()
+			}
+			card.history.push(history)
+			user.balance -= card.price;
+			user.deck = [...user.deck, card];
+			return zip(
+				from(
+					setDoc(cardReference, {
+						...card,
+						activeForSale: false,
+					}),
+				),
+				from(this.$user.updateUser(user)),
+			);
+		}
+
+		return throwError(()=>`Card not available for sale `);
+
 	}
 }
