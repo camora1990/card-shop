4daa12c3d27e4b9056c69c31528d6412e73eab51
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-01-31 14:51:33 -0500

    se agrega log al login

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -6,24 +6,28 @@ import {
 	signOut,
 	User
 } from '@firebase/auth';
-import { from, switchMap, tap, Observable } from 'rxjs';
+import { from, switchMap, tap, Observable, take } from 'rxjs';
 import { UserService } from '../../core/services/user.service';
 import { UserModel } from '../../core/domain/entities/user.model';
 import { Auth } from '@angular/fire/auth';
+import { LogService } from '../../core/services/log.service';
+import { LogTypes } from '../../core/domain/enums/logTypes.model';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class AuthService {
-	constructor(private $auth: Auth, private $user: UserService) {}
+	constructor(private $auth: Auth, private $user: UserService,private $log: LogService) {}
 
 	loginGoogle() {
 		return from(signInWithPopup(this.$auth, new GoogleAuthProvider())).pipe(
+			take(1),
 			switchMap((data) => {
 				return this.$user.getUser(data.user.uid);
 			}),
 			tap((user) => {
 				!user[0] && this.createUser(this.$auth.currentUser!);
+				this.$log.createLog(LogTypes.LOGIN,"Authenticated user")
 			}),
 		);
 	}
