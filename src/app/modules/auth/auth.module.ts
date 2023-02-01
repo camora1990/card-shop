24111bdfca05bd10c4251603d8d003808a7992b0
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-02-01 17:33:55 -0500

    se crea loading

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { OrganismsModule } from '../../shared/organisms/organisms.module';
 import { MoleculesModule } from '../../shared/molecules/molecules.module';
+import { AtomsModule } from '../../shared/atoms/atoms.module';
 
 @NgModule({
 	declarations: [LoginComponent],
-	imports: [CommonModule, AuthRoutingModule,OrganismsModule,MoleculesModule],
+	imports: [CommonModule, AuthRoutingModule,OrganismsModule,MoleculesModule,AtomsModule],
 })
 export class AuthModule {}
