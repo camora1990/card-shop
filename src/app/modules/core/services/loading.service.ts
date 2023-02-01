5a4abf799b9e18cb98a55f929a458bd06acf8ea3
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-02-01 17:34:41 -0500

    implementacion de loading

---
+++ src/app/modules/core/services/loading.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+
+  loading: Subject<boolean> = new Subject()
+  constructor() {
+    this.loading.next(false)
+   }
+}
