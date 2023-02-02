f3ebc50e39e646b66ec093da92e03694434e38ba
Camilo Morales Sanchez <camilo.morales@sofka.com.co>
2023-02-01 20:14:09 -0500

    implementacion mydeck

--- src/app/modules/shop/pages/my-deck/my-deck.component.ts
+++ src/app/modules/shop/pages/my-deck/my-deck.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../core/services/user.service';
+import { Card } from '../../../core/domain/entities/card.model';
+import { LoadingService } from '../../../core/services/loading.service';
+import { CardGroupBy } from 'src/app/modules/core/domain/entities/cardGroupBy.model';
+import { tap } from 'rxjs';
+import TransformCard from '../../../core/utils/transformCards';
 
 @Component({
   selector: 'app-my-deck',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyDeckComponent implements OnInit {
 
-  constructor() { }
+  groupCards: CardGroupBy[] = [];
+  constructor(private $user:UserService,private $loading:LoadingService) { }
 
   ngOnInit(): void {
+    this.$loading.loading.next(true)
+    this.$user.getUser(this.$user.currenUser?.uid!)
+    .pipe(tap((res)=> this.groupCards=TransformCard.transformData(res[0].deck)))
+    .subscribe({
+      next:()=>{
+        this.$loading.loading.next(false)
+
+      }
+    })
   }
 
 }
